59d8a2ad372a5d2df2703b39128602707f8ee92b
DenisKiarie3 <deniskiarie3@gmail.com>
2025-07-01 22:49:58 +0300

    Added Navbar.tsx and styled it responsively

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
-
+import { Route, Routes } from "react-router"
+import { Home } from "./pages/Home"
+import { Navbar } from "./components/Navbar"
 export default function App() {
   return(
-    <h1>Hello there</h1>
+    <div className="min-h-screen bg-black text-grey-100 transition-opacity duration-700 pt-20">
+      <Navbar/>
+      <div className="container mx-auto px-4 py-6">
+        <Routes>
+          <Route path="/" element={<Home/>} />
+        </Routes>
+      </div>
+    </div>
   )
 }
