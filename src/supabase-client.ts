d7a0e45781c1e0835bfc4544813ffb38d5b2efd6
DenisKiarie3 <deniskiarie3@gmail.com>
2025-07-08 19:34:49 +0300

    Supabase url in .env file

--- src/supabase-client.ts
+++ src/supabase-client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseURL = "https://govyzmletbzajfjrhrno.supabase.co"
+const supabaseURL = import.meta.env.VITE_SUPABASE_URL as string
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string
 
 export const supabase = createClient(supabaseURL, supabaseAnonKey)
